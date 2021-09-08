15f566522fd9b93fe374f2f259e5706a663a7f29
Rizer-Spiner <spiridon.roxana1999@gmail.com>
2021-09-08 20:02:32 +0200

    smth contains the dev profile credentials. Needs to be removed!

--- src/main/java/com/endregas/warriors/unitytesting/controllers/UnityRestController.java
+++ src/main/java/com/endregas/warriors/unitytesting/controllers/UnityRestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+
 @RestController
 public class UnityRestController {
 
@@ -23,7 +24,7 @@ public class UnityRestController {
     }
 
     @GetMapping("/getGreetings")
-    public Iterable<Greeting> getGreetings() {
+    public List<Greeting> getGreetings() {
         return greetingRepository.findAll();
     }
 }
