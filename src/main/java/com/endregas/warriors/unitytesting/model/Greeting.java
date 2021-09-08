be6436521a956b6a8fee2f90a0527161c04bc4e1
Rizer-Spiner <spiridon.roxana1999@gmail.com>
2021-09-08 11:51:55 +0200

    changed model

--- src/main/java/com/endregas/warriors/unitytesting/model/Greeting.java
+++ src/main/java/com/endregas/warriors/unitytesting/model/Greeting.java
@@ -1,17 +1,25 @@
 package com.endregas.warriors.unitytesting.model;
 
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
-@Builder
+
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Greeting {
-
+    @Id
     @GeneratedValue
     private Long id;
-
     private String message;
 
 }
