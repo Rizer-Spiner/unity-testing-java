64f0aecdac8c0f266c710cf650ee47ac6812247b
Rizer-Spiner <spiridon.roxana1999@gmail.com>
2021-09-08 10:38:56 +0200

    initial commit

---
+++ src/main/java/com/endregas/warriors/unitytesting/UnityTestingApplication.java
@@ -0,0 +1,13 @@
+package com.endregas.warriors.unitytesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnityTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnityTestingApplication.class, args);
+    }
+
+}
