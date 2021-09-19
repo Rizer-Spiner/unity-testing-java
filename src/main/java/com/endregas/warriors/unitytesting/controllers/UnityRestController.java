09c49ea100845284040a54f7b58cea1c99aabfbe
Rizer-Spiner <spiridon.roxana1999@gmail.com>
2021-09-19 12:53:35 +0200

    added endpoint for receiving data from unity game

--- src/main/java/com/endregas/warriors/unitytesting/controllers/UnityRestController.java
+++ src/main/java/com/endregas/warriors/unitytesting/controllers/UnityRestController.java
@@ -1,8 +1,11 @@
 package com.endregas.warriors.unitytesting.controllers;
 
 import com.endregas.warriors.unitytesting.model.Greeting;
+import com.endregas.warriors.unitytesting.model.PositionMetric;
 import com.endregas.warriors.unitytesting.repositories.GreetingRepository;
+import com.endregas.warriors.unitytesting.repositories.PositionRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,10 +15,12 @@ import java.util.List;
 public class UnityRestController {
 
     private GreetingRepository greetingRepository;
+    private PositionRepository positionRepository;
 
-    public UnityRestController(GreetingRepository greetingRepository)
+    public UnityRestController(GreetingRepository greetingRepository, PositionRepository positionRepository)
     {
         this.greetingRepository = greetingRepository;
+        this.positionRepository = positionRepository;
     }
 
     @GetMapping("/")
@@ -26,5 +31,18 @@ public class UnityRestController {
     @GetMapping("/getGreetings")
     public List<Greeting> getGreetings() {
         return greetingRepository.findAll();
+    }
+
+    @PostMapping("/postPosition")
+    public List<PositionMetric> postPosition(PositionMetric positionMetric)
+    {
+        positionRepository.save(positionMetric);
+        return positionRepository.findAll();
+    }
+
+    @GetMapping("/getPositions")
+    public List<PositionMetric> getPositions()
+    {
+        return positionRepository.findAll();
     }
 }
